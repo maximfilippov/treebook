1020353233fd0d540a93695062625be42b35d8aa
Maxim Filippov <max@loople.co>
2013-06-28 22:51:18 +0100

    Will I finally understand git???

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '9d6ca6cf3bf3d409e43939b0c1019a8271e3c568d4883f6b058f60b11ee7a8c733fa21b503a609a0b0c811049cf012b8f2185ecedcae123d6af9c5a5372531f6'
